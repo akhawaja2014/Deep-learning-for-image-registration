f2638d084968be76078e6252463b954de8cdca15
Arsalan KHAWAJA <tgiencov@MP3C-TGI-106-UBUNTU.sante.local>
2021-03-17 13:27:31 +0100

    fileadded

---
+++ VGG16.py
@@ -0,0 +1,38 @@
+from keras.preprocessing import image
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import preprocess_input
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+model = VGG16(weights='imagenet', include_top=False)
+model.summary()
+
+img_path = '/home/tgiencov/Registration Codes/Python image registration/feature_extraction/data/kagglecatsanddogs_3367a/PetImages/Dog/1.jpg'
+
+img = image.load_img(img_path, target_size=(224, 224))
+img_data = image.img_to_array(img)
+img_data = np.expand_dims(img_data, axis=0)
+img_data = preprocess_input(img_data)
+
+
+
+vgg16_feature = model.predict(img_data)
+
+print(vgg16_feature.shape)
+square = 8
+ix = 1
+for _ in range(square):
+	for _ in range(square):
+		# specify subplot and turn of axis
+		ax = plt.subplot(square, square, ix)
+		ax.set_xticks([])
+		ax.set_yticks([])
+		# plot filter channel in grayscale
+		plt.imshow(vgg16_feature[0, :, :, ix-1], cmap='gray')
+		ix += 1
+# show the figure
+plt.show()
+
+#plt.imshow(vgg16_feature[0,:,:,510])
+#plt.show()
